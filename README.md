7adc2dc7b82f7be470c75e58066b8e6d1ab2e520
Alex Troupe <adtroupe@uw.edu>
2015-11-21 19:41:50 -0800

    commented now yo. SMACK!

---
+++ README.md
@@ -0,0 +1,19 @@
+What is the URL of this project on your student web-server?
+
+
+
+
+Did you receive help from any other sources (classmates, etc.)? If so, please list who.
+
+	No, I did not receive any help.
+
+
+Approximately how many hours did it take you to complete this challenge?
+
+	It took approximately 30 hours to complete this project.
+
+
+Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better?
+
+	No problems were encountered during this project.
+
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+//My Angular App, using ui.router for multipage web architecture
 var myApp = angular.module('myApp', ['ui.router']);
 
+//Configuring the app for the different states of the multipage layout
 myApp.config(function($stateProvider) {
 	$stateProvider.state('home', {
 		url: '/',
@@ -27,15 +29,13 @@ myApp.config(function($stateProvider) {
 		controller: 'ContactController',
 	})
 })
-
+//The controller for the home page
 .controller('HomeController', function($scope) {
-
 })
-
+//The controller for the about page
 .controller('AboutController', function($scope) {
-
 })
-
+//The controller for the education page, makes two get requests and uses an angular filter
 .controller('EducationController', function($scope, $http, $filter) {
 	$http.get('data/school-info.json').success(function(response) {
 		$scope.schoolInfo = response;
@@ -43,41 +43,34 @@ myApp.config(function($stateProvider) {
 	$http.get('data/class-info.json').success(function(response) {
 		$scope.classInfo = response;
 	})
+	//Initializes what is seen on the page load
 	$scope.classCategory = 'ALL'
-
-	$(function () {
-  		
-	})
-
 })
-
+//The controller for the work page, makes a get request
 .controller('WorkController', function($scope, $http) {
 	$http.get('data/work-info.json').success(function(response) {
 		$scope.workInfo = response;
 	})
 })
-
+//The controller for the contact page
 .controller('ContactController', function($scope) {
-
 })
-
+//Creating the directive for the popovers on the education page
 .directive('popover', function(){
 	return function(scope, elem) {
 		elem.popover();
 	}
 });	
-
+//Changes the background visability based on the page's position
 $(document).ready(function() {
 	var scroll_pos = 0;
 	$(document).scroll(function() {
 		scroll_pos = $(this).scrollTop();
 		if (scroll_pos > 75) {
-			// $('.navbar').css('background-color', 'rgba(204, 204, 204, 0.9)');
 			$('.navbar').animate({
 				backgroundColor: "rgba(237, 245, 243, 0.9)"
 			}, 150);
 		} else {
-			// $('.navbar').css('background-color', 'transparent');
 			$('.navbar').animate({
 				backgroundColor: 'transparent'
 			}, 150);
